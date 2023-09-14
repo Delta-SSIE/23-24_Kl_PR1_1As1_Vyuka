06dab31c1e29f2dea3ee8d770031bd87bd21ac53
Jan Koupil <jan.koupil@gmail.com>
2023-09-14 18:40:18 +0200

    2023-09-14 Hello world

---
+++ 01-Uvod-01-Hello-World/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _01_Uvod_01_Hello_World
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

71780d542e2638c7b69075cb48b316c0c6efbac1
Jan Koupil <jan.koupil@gmail.com>
2023-09-25 13:27:16 +0200

    2023-09-25 komentování kódu, prohození proměnných

---
+++ 01-Uvod-07-Prumer/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _01_Uvod_07_Prumer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //vyzveme uživatele
+            Console.WriteLine("Zadej dvě čísla, já spočítám průměr:");
+
+            //načteme první číslo (řetězec)
+            string vstup1 = Console.ReadLine();
+
+            //načteme druhé číslo (řetězec)
+            string vstup2 = Console.ReadLine();
+
+            //převedeme oba vstupy na číslo
+            double cislo1 = double.Parse(vstup1);
+            double cislo2 = double.Parse(vstup2);
+
+            //vypočítáme průměr
+            double prumer = (cislo1 + cislo2) / 2;
+
+            //vypíšme výsledek
+            Console.WriteLine("Průměr je " + prumer);
+        }
+    }
+}
---
+++ 01-Uvod-08-Prohozeni/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _01_Uvod_08_Prohozeni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string hrnek = "mléko";
+            string sklenice = "džus";
+
+            //jak prohodíme? Jako na kuchyňské lince
+
+            //potřebuju třetí - dočasnou proměnnou
+            string tmp = hrnek; //přeleju do dočasné nádoby z hrnka
+            hrnek = sklenice; //přeleju do hrnka ze sklenice
+            sklenice = tmp; //přeleju do sklenice to, co jsem si odložil
+
+            Console.WriteLine($"Ve sklenici je {sklenice}, v hrnku {hrnek}");
+        }
+    }
+}

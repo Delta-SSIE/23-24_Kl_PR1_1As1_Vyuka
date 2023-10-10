4c1848ea141b879ad7f90e78528dda702b2a195e
Jan Koupil <jan.koupil@gmail.com>
2023-10-10 10:13:35 +0200

    2023-10-09 podminky a Case

---
+++ 02-Podminky-09-Switch/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Text.Json;
+
+namespace _02_Podminky_09_Switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Jaké je číslo dnešního dne (1-7): ");
+            int cisloDne = int.Parse(Console.ReadLine());
+
+            switch (cisloDne)
+            {
+                case 1:
+                    Console.WriteLine("pondělí");
+                    break;
+
+                case 2:
+                    Console.WriteLine("úterý");
+                    break;
+
+                case 3:
+                    Console.WriteLine("středa");
+                    break;
+
+                case 4:
+                    Console.WriteLine("čtvrtek");
+                    break;
+
+                case 5:
+                    Console.WriteLine("pátek");
+                    break;
+
+                case 6:
+                    Console.WriteLine("sobota");
+                    break;
+
+                case 7:
+                    Console.WriteLine("neděle");
+                    break;
+
+                default: //všechny ostatní případy
+                    Console.WriteLine("Píšeš nesmysly");
+                    break;
+            }
+
+            switch (cisloDne)
+            {
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("není víkend");
+                    break;
+                case 6:
+                case 7:
+                    Console.WriteLine("je víkend");
+                    break;
+
+            }
+
+        }
+    }
+}
---
+++ 02-Podminky-10-Znaky/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json;
+
+namespace _02_Podminky_10_Znaky
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //změna velikosti znaku
+            //char znak = 'c';
+            //char maly = char.ToLower(znak); //změní znak na malé písmeno
+            //Console.WriteLine(maly);
+
+            char znak = '5';
+
+            int cisloZnaku = znak; //zjištění typu znaku přes Ascii
+            Console.WriteLine(cisloZnaku);
+            if (cisloZnaku >= 48 && cisloZnaku <= 57)
+                Console.WriteLine("Je to číslice");
+
+            if (char.IsDigit(znak)) //zjištění pomocí metod třídy char (IsDigit, IsAscii, ...)
+            {
+                Console.WriteLine("Je to číslice");
+            }
+        }
+    }
+}

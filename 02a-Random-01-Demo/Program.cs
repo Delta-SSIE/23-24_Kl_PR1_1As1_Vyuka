9eef1b76f4a42fa0a582ee1b2c91dd42d3e9df86
Jan Koupil <jan.koupil@gmail.com>
2023-10-16 11:01:31 +0200

    2023-10-16 Random - uvod, demo

---
+++ 02a-Random-01-Demo/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _02a_Random_01_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //vytvořím generátor náhodných čísel
+            Random random = new Random();
+            //Random random = new Random(1568794521); //vložím "semínko" - seed, pak se budou čísla při příštím spuštění opakovat
+
+            Console.WriteLine(random.NextDouble()); //vytvoří "náhodné" číslo v intervalu <0, 1)
+            Console.WriteLine(random.NextDouble());
+            Console.WriteLine(random.NextDouble());
+            Console.WriteLine(random.NextDouble());
+
+            int nahodne = random.Next(1,7); //1 ... první, které chci
+                                            //7 ... první, které už nechci
+
+            Console.WriteLine(nahodne);
+            
+            Console.WriteLine(random.Next(1, 7));
+            Console.WriteLine(random.Next(1, 7));
+            Console.WriteLine(random.Next(1, 7));
+            Console.WriteLine(random.Next(1, 7));
+            Console.WriteLine(random.Next(1, 7));
+            Console.WriteLine(random.Next(1, 7));
+            Console.WriteLine(random.Next(1, 7));
+            Console.WriteLine(random.Next(1, 7));
+            Console.WriteLine(random.Next(1, 7));
+            Console.WriteLine(random.Next(1, 7));
+        }
+    }
+}
---
+++ 02a-Random-02-Mince/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _02a_Random_02_Mince
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int hod = random.Next(0, 2);
+            if (hod == 0)
+                Console.WriteLine("Panna");
+            else
+                Console.WriteLine("Vorel");
+
+        }
+    }
+}

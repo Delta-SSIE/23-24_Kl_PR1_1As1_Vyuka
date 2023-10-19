eb1296abb27a9eda6deb6dab12f11cd09218fb7d
Jan Koupil <jan.koupil@delta-skola.cz>
2023-10-19 12:58:36 +0200

    2023-10-19 Kamen-nuzky-papir

---
+++ 02a-Random-03-KNP/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace _02a_Random_03_KNP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Budeme hrát Kámen/Nůžky/Papír");
+
+            //načtení figury hráče
+            Console.WriteLine("Zvol figuru (k/n/p): ");
+            char znakHrac = Console.ReadKey().KeyChar;
+            Console.WriteLine(); //to je jen odřádkování za stiskem klávesy
+
+            znakHrac = char.ToLower(znakHrac);
+
+            if (znakHrac != 'k' && znakHrac != 'n' && znakHrac != 'p')
+            {
+                Console.WriteLine("Neplatný symbol, ukončuji hru.");
+                return;
+            }
+
+
+            //načtení figury PC
+            Random random = new Random();
+            int nahCislo = random.Next(0, 3);
+            
+            char znakPC = '\0'; //naplním jakoukoli výchozí hodnotou
+            switch (nahCislo)
+            {
+                case 0:
+                    znakPC = 'k';
+                    break;
+                case 1:
+                    znakPC = 'n';
+                    break;
+                case 2:
+                    znakPC = 'p';
+                    break;
+                //default: //jen aby si nemyslel, že ve znakPC nemůže nic být
+                //    znakPC = '\0';
+                //    break;
+            }
+
+
+            //vyhodnocení
+            string vysledek;
+            if (znakPC == znakHrac)
+            { 
+                vysledek = "Remíza";
+            }
+            else if (
+                (znakPC == 'k' && znakHrac == 'n')
+                || (znakPC == 'n' && znakHrac == 'p')
+                || (znakPC == 'p' && znakHrac == 'k')
+            )
+            {
+                vysledek = "Vítězí počítač";
+            }
+            else
+            {
+                vysledek = "Vítězí hráč";
+            }
+
+            //závěr
+            Console.WriteLine($"{vysledek} ({znakHrac} : {znakPC}).");
+        }
+    }
+}

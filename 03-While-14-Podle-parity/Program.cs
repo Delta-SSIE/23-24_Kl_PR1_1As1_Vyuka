66a4dce76d8b9b0422205c40960e78f0a33d2928
Jan Koupil <jan.koupil@gmail.com>
2023-11-12 15:43:50 +0100

    2023-11-10 While (parita, serie sestek)

---
+++ 03-While-14-Podle-parity/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _03_While_14_Podle_parity
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int sude = 0;
+            int liche = 0;
+            int cislo;
+            do
+            {
+                Console.WriteLine("Zadej prirozene cislo");
+                string vstup = Console.ReadLine();
+
+                while (!int.TryParse(vstup, out cislo))
+                {
+                    Console.WriteLine("Zadej prosim prirozene cislo");
+                    vstup = Console.ReadLine();
+                }
+                if (cislo > 0)
+                {
+                    if (cislo % 2 == 0)
+                    {
+                        sude++;
+                    }
+                    else
+                    {
+                        liche++;
+                    }
+                }
+            }
+            while (cislo > 0);
+
+            Console.WriteLine($"Zadal jsi {sude} sudych cisel a {liche} lichych cisel");
+
+        }
+    }
+}
---
+++ 03-While-15-Serie-sestek/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _03_While_15_Serie_sestek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pocetHodu = 1000;
+            int nejdelsiSerie = 0;
+            int delkaTetoSerie = 0;
+            Random rnd = new Random();
+
+            int i = 0;
+            while(i < pocetHodu)
+            {
+                int cislo = rnd.Next(1, 7);
+                if (cislo == 6)
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+                Console.Write(cislo);
+                
+                if (cislo == 6)
+                    delkaTetoSerie++;
+                else               
+                    delkaTetoSerie = 0;                
+
+                if (nejdelsiSerie < delkaTetoSerie)
+                    nejdelsiSerie = delkaTetoSerie;
+                
+                i++;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Nejdelsi serie sestek mela delku " + nejdelsiSerie);
+        }
+    }
+}

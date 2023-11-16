b7d67bfd015c19e0018c1e9a592dfbb7ed1b8735
Jan Koupil <jan.koupil@gmail.com>
2023-11-16 19:57:39 +0100

    2023-11-16 Cyklus For

---
+++ 04-For-01-Demo/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _04_For_01_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int i = 0;
+            //while (i < 10)
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+            // i zůstane jako aktivní proměnná i po skončení cyklu
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            // napočítat po 10 do 100
+            for (int i = 0; i < 101; i += 10)
+            {
+                Console.WriteLine(i);
+            }
+
+            // od 100 do nuly
+            for (int i = 100; i >= 0; i -= 10)
+            {
+                Console.WriteLine(i);
+            }
+
+
+
+        }
+    }
+}
---
+++ 04-For-02-retezec/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _04_For_02_retezec
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string veta = "Kdy už bude konec?";
+            Console.WriteLine(veta.Length);
+            Console.WriteLine(veta[0]); //prvni znak
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine(veta[veta.Length - 1]); //posledni znak - první byl [0]
+
+            for (int i = 0; i < veta.Length; i++)
+            {
+                char znak = veta[i];
+                if (znak != ' ')
+                    Console.Write(znak);
+            }
+            Console.WriteLine();
+
+
+            for (int i = veta.Length - 1; i >= 0; i--)
+            {
+                Console.Write(veta[i]);
+            }
+            Console.WriteLine();
+        }
+    }
+}

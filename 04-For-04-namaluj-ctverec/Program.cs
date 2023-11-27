91dc2a45bdbb10247faceefc6ffb571a94145caa
Jan Koupil <jan.koupil@gmail.com>
2023-11-27 12:03:36 +0100

    2023-11-27 Kreslení do konzole

---
+++ 04-For-04-namaluj-ctverec/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _04_For_04_namaluj_ctverec
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Jaká bude strana čtverce?");
+
+            //int strana;
+            //while (!int.TryParse(Console.ReadLine(), out strana) || strana < 0)
+            //{
+            //    Console.WriteLine("Zadej kladné celé číslo");
+            //}
+
+            //for (int y = 0; y < strana; y++)
+            //{
+            //    for (int x = 0; x < strana; x++)
+            //    {
+            //        Console.Write('#');
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            int sirka = 3;
+            int vyska = 5;
+            char znak = '#';
+           
+
+            for (int y = 0; y < vyska; y++)
+            {
+                for (int x = 0; x < sirka; x++)
+                {
+                    Console.Write(znak);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 04-For-05-Trojuhelnik-b/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _04_For_05_Trojuhelnik_b
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int vyska = 5;
+            char znak = '@';
+
+            for (int y = 0; y < vyska; y++)
+            {
+                for (int x = 0; x < y + 1; x++)
+                {
+                    Console.Write(znak);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

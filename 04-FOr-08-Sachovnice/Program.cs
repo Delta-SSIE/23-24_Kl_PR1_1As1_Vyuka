df1da0c598e10b6d318911c9087a2449d01c3d37
Jan Koupil <jan.koupil@gmail.com>
2023-11-30 13:02:57 +0100

    2023-11-30 Vnořené for-cykly, šachy

---
+++ 04-FOr-08-Sachovnice/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace _04_FOr_08_Sachovnice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pozice = "D3";
+
+            int radek = pozice[1] - '0';
+            int sloupec = pozice[0] - 'A' + 1;
+
+            //nakreslit šachovnici
+            int max = 8;
+            bool tmava = false;
+
+            Console.Write("  ");
+            for (int i = 0; i < max; i++)
+            {
+                Console.Write( (char)('A' + i) + " ");
+            }
+            Console.WriteLine();
+
+            for (int y = 8; y > 0; y--)
+            {
+                Console.Write($"{y} ");
+
+                for (int x = 1; x <= max; x++)
+                {
+                    bool stojiTuFigura = y == radek && x == sloupec;
+                    //bool ohrozenePole = y == radek || x == sloupec; //pro vez
+                    
+                    int rozdilRadek = Math.Abs(y - radek);
+                    int rozdilSloupec = Math.Abs(x - sloupec);
+
+                    //bool ohrozenePole = rozdilRadek < 2 && rozdilSloupec < 2; //kral
+                    //bool ohrozenePole = rozdilRadek == rozdilSloupec; //strelec
+                    bool ohrozenePole = (y == radek || x == sloupec) || rozdilRadek == rozdilSloupec; //dama
+                    //bool ohrozenePole = (rozdilRadek == 2 && rozdilSloupec == 1) || (rozdilRadek == 1 && rozdilSloupec == 2); //jezdec
+
+
+                    if (!tmava)
+                    {
+                        if (stojiTuFigura)
+                            Console.BackgroundColor = ConsoleColor.Yellow;
+                        else if (ohrozenePole)
+                            Console.BackgroundColor = ConsoleColor.Magenta;
+                        else
+                            Console.BackgroundColor = ConsoleColor.White;
+                    }
+                    else
+                    {
+                        if (stojiTuFigura)
+                            Console.BackgroundColor = ConsoleColor.DarkYellow;
+                        else if (ohrozenePole)
+                            Console.BackgroundColor = ConsoleColor.DarkMagenta;
+                    }
+
+                    Console.Write("  ");
+                    Console.BackgroundColor = ConsoleColor.Black;
+
+
+                    tmava = !tmava;
+                }
+                tmava = !tmava;
+
+                Console.WriteLine($" {y}");
+            }
+            Console.Write("  ");
+            for (int i = 0; i < max; i++)
+            {
+                Console.Write((char)('A' + i) + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 04-For-07-algebrogram/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace _04_For_07_algebrogram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //   SEND
+            // + MORE
+            //  MONEY
+
+            int S, E, N, D, M, O, R, Y;
+
+            for (S = 1; S < 10; S++)  //od 1 - na začátku nesmí být 0
+            {
+                for (E = 0; E < 9; E++)
+                {
+                    if (E == S) continue; //přeskoč, protože číslo už je použité
+                    
+                    for (N = 0; N < 9; N++)
+                    {
+                        if (N == S || N == E) continue;
+
+                        for (D = 0; D < 9; D++)
+                        {
+                            if (D == S || D == E || D == N) continue;
+
+                            for (M = 1; M < 9; M++)
+                            {
+                                if (M == S || M == E || M == N || M == D) continue;
+
+                                for (O = 0; O < 9; O++)
+                                {
+                                    if (O == S || O == E || O == N || O == D || O == M) continue;
+
+                                    for (R = 0; R < 9; R++)
+                                    {
+                                        if (R == S || R == E || R == N || R == D || R == M || R == 0) continue;
+
+                                        for (Y = 0; Y < 9; Y++)
+                                        {
+                                            if (Y == S || Y == E || Y == N || Y == D || Y == M || Y == 0 || Y == R) continue;
+
+                                            if (
+                                                                S * 1_000 + E * 100 + N * 10 + D
+                                                              + M * 1_000 + O * 100 + R * 10 + E
+                                                == M * 10_000 + O * 1_000 + N * 100 + E * 10 + Y
+                                                )
+                                            {
+                                                Console.WriteLine($"   {S}{E}{N}{D}");
+                                                Console.WriteLine($" + {M}{O}{R}{E}");
+                                                Console.WriteLine($"= {M}{O}{N}{E}{Y}");
+                                                Console.WriteLine("-------");
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }            
+            }
+
+
+
+
+        }
+    }
+}

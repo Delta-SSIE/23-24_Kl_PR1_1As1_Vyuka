0af73ffc011137cbefa93bdda56dc3dcabe81c08
Jan Koupil <jan.koupil@gmail.com>
2024-01-08 11:34:26 +0100

    2024-08-01 operace s polem

---
+++ 05-Array-07-Zarotuj-pole/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _05_Array_07_Zarotuj_pole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] cisla = { -6, -7, -1, 3, 0, 1, 3, 5, 9, 8, 10 };
+            int posun = 3;
+
+            int pocet = cisla.Length;
+            int[] posunuto = new int[pocet];
+            for (int odkud = 0; odkud < pocet; odkud++)
+            {
+                int kam = odkud - posun;
+                if (kam < 0)
+                    kam += pocet;
+                posunuto[kam] = cisla[odkud];
+            }
+
+            Console.WriteLine(string.Join(' ', posunuto));
+        }
+    }
+}
---
+++ 05-Array-08-Presun-nuly-na-konec/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _05_Array_08_Presun_nuly_na_konec
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] cisla = { 1, -5, 0, 4, 0, 7, 12, 0, 4 };
+            int[] cisla2 = new int[cisla.Length];
+
+            int kam = 0;
+            for (int odkud = 0; odkud < cisla.Length; odkud++)
+            {
+                int cislo = cisla[odkud];
+                if (cislo != 0)
+                {
+                    cisla2[kam] = cislo;
+                    kam++;
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", cisla2));
+
+        }
+    }
+}
